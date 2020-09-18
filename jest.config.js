5a482f51b3e5e37a9c9f4671db248aae2b1f696c
rafael.fumegalli <rafaelxxc@gmail.com>
2020-09-18 18:34:13 -0300

    test: Add first functional test

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+
+const root = resolve(__dirname);
+
+module.exports = {
+  rootDir: root,
+  displayName: 'root-tests',
+  testMatch: ['<rootDir>/src/**/*.test.ts'], // for unit tests. End-to-end tests will be at /test folder
+  testEnvironment: 'node',
+  clearMocks: true,
+  preset: 'ts-jest',
+  moduleNameMapper: { // to use import with @
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1',
+  },
+};
