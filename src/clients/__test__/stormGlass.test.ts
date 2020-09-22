768630e34b66745074a6abea4ad6cb030e8267ce
rafael.fumegalli <rafaelxxc@gmail.com>
2020-09-22 17:30:42 -0300

    refact: Add tpescript + jest improvement

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,20 +1,20 @@
 import { StormGlass } from '@src/clients/stormGlass';
-import axios from 'axios';
+import axios, { AxiosStatic } from 'axios';
 import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
 import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<AxiosStatic>;
+
   it('should return the normalized forecast from StormGlass service', async () => {
     const lat = -33.3;
     const lng = 151.5;
 
-    axios.get = jest
-      .fn()
-      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
 
     const response = await stormGlass.fetchPoints(lat, lng);
 
