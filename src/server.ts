5391dc7f5a891f620154a7df4101ac8a9e81280a
rafael.fumegalli <rafaelxxc@gmail.com>
2020-09-22 11:57:12 -0300

    feat: Add prettier setup

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,7 @@ import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
 
-
 export class SetupServer extends Server {
-
   constructor(private port = 3000) {
     super();
   }
--- test/globals.d.ts
+++ test/globals.d.ts
@@ -2,6 +2,6 @@ declare namespace NodeJS {
   interface Global {
     // Import inline to override global types.
     // https://stackoverflow.com/a/51114250
-    testRequest: import('supertest').SuperTest<import('supertest').Test>
+    testRequest: import('supertest').SuperTest<import('supertest').Test>;
   }
 }
