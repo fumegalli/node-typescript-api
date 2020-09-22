a6446f2578bb91d20f3595158ab2b93509132ef3
rafael.fumegalli <rafaelxxc@gmail.com>
2020-09-22 17:21:05 -0300

    feat: Add stormGlass clint & Its unit test

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,23 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from StormGlass service', async () => {
+    const lat = -33.3;
+    const lng = 151.5;
+
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+
+    const stormGlass = new StormGlass(axios);
+
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
